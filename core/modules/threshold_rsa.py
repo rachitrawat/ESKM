03b0fae749ebe1683e59d6f3d78e5473215226a7
rachitrawat <rachit.rc.rawat@gmail.com>
2018-10-18 19:02:14 +0530

    refactor 2

--- core/modules/threshold_rsa.py
+++ core/modules/threshold_rsa.py
@@ -1,29 +1,24 @@
-import hashlib
-import math
-
 from core.modules import misc, lagrange_interpolation as lp
 
 
-def compute_sig(dict, digest, n, N, e):
-    x_dict = {}
+def compute_threhold_sig(node_share_dict, digest, delta, n, e):
+    x_i = {}
     w = 1
 
-    delta = math.factorial(n)
+    for node, share in node_share_dict.items():
+        x_i[node] = misc.square_and_multiply(digest, 2 * delta * share, n)
 
-    for node, share in dict.items():
-        x_dict[node] = misc.square_and_multiply(digest, (2 * delta * share), N)
+    for node, x_i in x_i.items():
+        lambda_i = int(lp.lambda_eval_s(node, node_share_dict.keys(), delta))
+        if lambda_i < 0:
+            x_i = misc.multiplicative_inverse(x_i, n)
+        lambda_i = abs(lambda_i)
+        w *= misc.square_and_multiply(x_i, 2 * lambda_i, n)
 
-    for node, x_val in x_dict.items():
-        lambda_i_s = int(lp.lambda_eval_s(node, dict.keys(), delta))
-        if lambda_i_s < 0:
-            x_val = misc.multiplicative_inverse(x_val, N)
-        lambda_i_s = abs(lambda_i_s)
-        w *= misc.square_and_multiply(x_val, 2 * lambda_i_s, N)
-
-    one, a, b = misc.multiplicative_inverse(4 * (delta ** 2), e, True)
+    one, a, b = misc.multiplicative_inverse(4 * delta ** 2, e, True)
     b = (1 - (4 * (delta ** 2) * a)) // e
     if b < 0:
-        digest = misc.multiplicative_inverse(digest, N)
-    sig = misc.square_and_multiply(w, a, N) * misc.square_and_multiply(digest, b, N)
+        digest = misc.multiplicative_inverse(digest, n)
+    threshold_sig = misc.square_and_multiply(w, a, n) * misc.square_and_multiply(digest, b, n)
 
-    return sig, digest
+    return threshold_sig % n
